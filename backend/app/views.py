c1e6446c8185d9fb68b6af3f275d9094a5e66538
Barracoda130 <lcompton-burnett@hotmail.com>
2024-03-03 17:04:43 +0000

    fixed views.py bug

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import render_template, url_for, redirect, flash, request
-import json
 
 
 @app.route('/', methods=['GET'])
@@ -10,7 +9,7 @@ def home():
 @app.route('/add', methods=['POST'])
 def add():
     post_data = request.get_json()
-    response_object = json.dumps({'status': 'success'})
+    response_object = {'status': 'success'}
     response_object['addition'] = post_data['a'] + post_data['b']
     return response_object, 201
 
