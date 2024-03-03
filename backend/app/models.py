19db1fc77e53460f0170e0c0616b8097095974f8
Barracoda130 <lcompton-burnett@hotmail.com>
2024-03-03 14:16:24 +0000

    Half way through writing db tests

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,13 @@
-from app import db
+from app import db, app
 import bcrypt
 from sqlalchemy import or_
+
+# THIS DOESN'T BELONG HERE BUT IDK WHERE TO PUT IT
+def db_add(*args):
+    with app.app_context():
+        for arg in args:
+            db.session.add(arg)
+        db.session.commit()
 
 class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +21,22 @@ class Friend(db.Model):
     
     user_1 = db.relationship("User", foreign_keys=[user_1_id])
     user_2 = db.relationship("User", foreign_keys=[user_2_id])
+    
+    def __init__(self, u1=None, u2=None):
+        """
+        Can be used to more easily define a friendship.
+        Either the user ID or the user instance can be passed and it will set those values.
+
+        Args:
+            u1 (int/User, optional): The ID or instance of the first user in the friendship. Defaults to None.
+            u2 (int/User, optional): The ID or instance of the second user in the friendship. Defaults to None.
+        """
+        if type(u1) == int:
+            self.user_1_id = u1
+            self.user_2_id = u2
+        else:
+            self.user_1 = u1
+            self.user_2 = u2
 
 class FriendRequest(db.Model):
     from_user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
@@ -21,13 +44,32 @@ class FriendRequest(db.Model):
     
     from_user = db.relationship("User", foreign_keys=[from_user_id])
     to_user = db.relationship("User", foreign_keys=[to_user_id])
+    
+    def __init__(self, from_user=None, to_user=None):
+        """
+        Can be used to more easily define a friend request.
+        Either the user ID or the user instance can be passed and it will set those values.
+
+        Args:
+            from_user (int/User, optional): The ID or instance of the FROM user in the request. Defaults to None.
+            to_user (int/User, optional): The ID or instance of the TO user in the request. Defaults to None.
+        """
+        if type(from_user) == int:
+            self.from_user_id = from_user
+            self.to_user_id = to_user
+        else:
+            self.from_user = from_user
+            self.to_user = to_user
 
 class MembershipPlan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), unique=True, nullable=False)
     cost = db.Column(db.Float)
     payment_regularity = db.Column(db.String(32))
-    members = db.relationship("User", backref="membership_plan", lazy="dynamic")
+    
+    # lazy="dynamic" means that to use this parameter, you need to call methods on it
+    # like m.members.all() or m.members.first() etc.
+    members = db.relationship("User", back_populates="membership", lazy="dynamic")
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,44 +85,125 @@ class User(db.Model):
 
     @staticmethod
     def authenticate(username, password):
+        """
+        Gets the user with the given username and password, none otherwise.
+        Is a static method so is called using User.authenticate, not called on an
+        instance of the User class.
+
+        Args:
+            username (str): Username of the potential user
+            password (str): Password of the potential user
+
+        Returns:
+            (User): The user if found, None otherwise
+        """
         result = User.query.filter_by(username=username).all()
         if len(result) > 0:
             u = result[0]
             encode = password.encode('utf-8')
             if bcrypt.checkpw(encode, u.password):
                 return u
+        return None
             
     def friends(self):
-        table = Friend.query.filter(or_(Friend.fk_user_1 == self.id, Friend.fk_user_2 == self.id)).all()
-        friends = []
+        """
+        Get all the friends of the user.
+
+        Returns:
+            (set): A set of all the user's friends (as User objects)
+        """
+        table = Friend.query.filter(or_(Friend.user_1 == self, Friend.user_2 == self)).all()
+        friends = set()
         for entry in table:
-            if entry.fk_user_1 == self.id:
-                friends.append(entry.user_2)
+            if entry.user_1 == self:
+                friends.add(entry.user_2)
             else:
-                friends.append(entry.user_1)
+                friends.add(entry.user_1)
         return friends
     
     def outgoint_friend_requests(self):
-        return
+        table = FriendRequest.query.filter(FriendRequest.from_user == self).all()
+        outgoing_requests = set()
+        for entry in table:
+            outgoing_requests.add(entry.to_user)
+            
+        return outgoing_requests
     
-    def incoming_friend_request(self):
-        return
+    def incoming_friend_requests(self):
+        table = FriendRequest.query.filter(FriendRequest.to_user == self).all()
+        incoming_requests = set()
+        for entry in table:
+            incoming_requests.add(entry.from_user)
+            
+        return incoming_requests
+
+class UserInGroup(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"), primary_key=True)
+    
+    user = db.relationship("User", foreign_keys=[user_id])
+    group = db.relationship("Group", foreign_keys=[group_id])
+    
+    def __init__(self, user=None, group=None):
+        """
+        Can be used to more easily define a user in a group.
+        Either the user+group ID or the user+group instance can be passed and it will set those values.
+
+        Args:
+            user (int/User, optional): The ID or instance of the user in the group. Defaults to None.
+            group (int/Group, optional): The ID or instance of the group for the user. Defaults to None.
+        """
+        if type(user) == int:
+            self.user_id = user
+            self.group_id = group
+        else:
+            self.user = user
+            self.group = group
+
+class RouteInGroup(db.Model):
+    route_id = db.Column(db.Integer, db.ForeignKey("route.id"), primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"), primary_key=True)
+    
+    route = db.relationship("Route", foreign_keys=[route_id])
+    group = db.relationship("Group", foreign_keys=[group_id])
+    
+    def __init__(self, route=None, group=None):
+        """
+        Can be used to more easily define a user in a group.
+        Either the route+group ID or the route+group instance can be passed and it will set those values.
+
+        Args:
+            user (int/User, optional): The ID or instance of the user in the group. Defaults to None.
+            group (int/Group, optional): The ID or instance of the group for the user. Defaults to None.
+        """
+        if type(route) == int:
+            self.route_id = route
+            self.group_id = group
+        else:
+            self.route = route
+            self.group = group
 
 class Route(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text)
     name = db.Column(db.String(128))
     exercise_type = db.Column(db.String(64))
-    fk_user = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    
+    user = db.relationship("User", foreign_keys=[user_id])
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
+    
+    members = db.relationship('User', secondary=UserInGroup)
+    
+    def members_func(self):
+        table = UserInGroup.query.filter(FriendRequest.from_user == self).all()
+        outgoing_requests = set()
+        for entry in table:
+            outgoing_requests.add(entry.to_user)
+            
+        return outgoing_requests
 
-class UserInGroup(db.Model):
-    fk_user = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-    fk_group = db.Column(db.Integer, db.ForeignKey("group.id"), primary_key=True)
 
-class RouteInGroup(db.Model):
-    fk_route = db.Column(db.Integer, db.ForeignKey("route.id"), primary_key=True)
-    fk_group = db.Column(db.Integer, db.ForeignKey("group.id"), primary_key=True)
--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -4,6 +4,12 @@ from app import app, db
 import bcrypt
 import string, random
 
+"""
+CURRENTLY TESTS HAVE NO EDGE CASES/ERROR CHECKING!
+"""
+
+# ----------------------------------------------------------------
+# Useful functions NOT TESTS
 def hash_pwd(password):
     encode = password.encode('utf-8')
     salt = bcrypt.gensalt()
@@ -20,6 +26,12 @@ def create_user(username, password):
         
     return u
 
+def delete_all(c):
+    with app.app_context():
+        for user in c.query.all():
+            db.session.delete(user)
+        db.session.commit()
+
 def delete_user(username, password):
     with app.app_context():
         result = User.query.filter_by(username=username).all()
@@ -29,12 +41,18 @@ def delete_user(username, password):
             if bcrypt.checkpw(encode, u.password):
                 db.session.delete(u)
         db.session.commit()
-
+    
 def get_random_string(n):
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for x in range(n))
 
+# ----------------------------------------------------------------
+# TESTS START
+
 def test_user_creation():
+    """
+    Creates 10 users, and then tries to authenticate each one.
+    """
     for i in range(1):
         uname = get_random_string(10)
         pwd = get_random_string(15)
@@ -44,11 +62,101 @@ def test_user_creation():
         delete_user(uname, pwd)
         
 def test_friends():
-    u1 = create_user(get_random_string(10), get_random_string(15))
-    u2 = create_user(get_random_string(10), get_random_string(15))
-    f = Friend(u1, u2)
-    with app.app_context():
-        db.session.add(f)
-        db.session.commit()
+    """
+    Tests frienships by:
+        - Creates 3 users
+        - u1 friends u2 and u3
+        - Check for all 3 that they have the correct friends
+    """
+    delete_all(Friend)
+    delete_all(User)
+    u1 = create_user('test3', 'pwd')
+    u2 = create_user('test4', 'pwd')
+    u3 = create_user('test5', 'pwd')
+    
+    f1 = Friend(u1, u2)
+    f2 = Friend(u1, u3)
+    
+    db_add(f1, f2)
         
-    print(u1.get_friends())
+    u1 = User.authenticate('test3', 'pwd')
+    u2 = User.authenticate('test4', 'pwd')
+    u3 = User.authenticate('test5', 'pwd')
+    
+    assert u1.friends() == {u2, u3}
+    assert u2.friends() == {u1}
+    assert u3.friends() == {u1}
+ 
+def test_friend_requests():
+    """
+    Tests friend requests by:
+        - Creates 3 users
+        - u1 requests u2
+        - u2 requests u3
+        - checks all incoming and outgoing fields for all users
+    """
+    delete_all(User)
+    delete_all(FriendRequest)
+    u1 = create_user('u1', 'pwd')
+    u2 = create_user('u2', 'pwd')
+    u3 = create_user('u3', 'pwd')
+    
+    r1 = FriendRequest(u1, u2)
+    r2 = FriendRequest(u2, u3)
+    
+    db_add(r1, r2)
+        
+    u1 = User.authenticate('u1', 'pwd')
+    u2 = User.authenticate('u2', 'pwd')
+    u3 = User.authenticate('u3', 'pwd')
+    
+    assert u1.outgoint_friend_requests() == {u2}
+    assert u1.incoming_friend_requests() == set()
+    assert u2.outgoint_friend_requests() == {u3}
+    assert u2.incoming_friend_requests() == {u1}
+    assert u3.outgoint_friend_requests() == set()
+    assert u3.incoming_friend_requests() == {u2}
+    
+def test_membership():
+    """
+    Test membership by:
+        - Creates 3 membership categories
+        - Creates 4 users
+        - Assigns users to categories
+        - Checks both that the MembershipPlan.members.all() contains the right users
+        - And User.membership is the correct membership
+    """
+    delete_all(MembershipPlan)
+    delete_all(User)
+    
+    m1 = MembershipPlan()
+    m1.name = "Test"
+    m2 = MembershipPlan()
+    m2.name = "Test2"
+    m3 = MembershipPlan()
+    m3.name = "Test3"
+    
+    u1 = create_user('u1', 'pwd')
+    u2 = create_user('u2', 'pwd')
+    u3 = create_user('u3', 'pwd')
+    u4 = create_user('u4', 'pwd')
+    
+    u1.membership = m1
+    u2.membership = m1
+    u3.membership = m1
+    u4.membership = m2
+    
+    db_add(m1, m2, m3, u1, u2, u3, u4)
+        
+    m1, m2, m3 = MembershipPlan.query.all()
+    u1 = User.authenticate('u1', 'pwd')
+    u2 = User.authenticate('u2', 'pwd')
+    u3 = User.authenticate('u3', 'pwd')
+    u4 = User.authenticate('u4', 'pwd')
+    
+    assert m1.members.all() == [u1, u2, u3]
+    assert u1.membership == m1
+    assert u2.membership == m1
+    assert m2.members.all() == [u4]
+    assert m3.members.all() == []
+    
