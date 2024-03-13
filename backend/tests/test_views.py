d6d66da198d2e66dc33d169c568dd94038a425cb
Barracoda130 <lcompton-burnett@hotmail.com>
2024-03-13 10:50:53 +0000

    Removed view testing as they wont' work with new authentication

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -1,14 +1,3 @@
 from app import app
 import pytest
 from tests.conf import client
-
-
-def test_home_route(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Greetings from the API!'
-
-def test_add_route(client):
-    response = client.post('/add', json={'a': 5, 'b': 3})
-    assert response.status_code == 201
-    assert response.json == {'status': 'success', 'addition': 8}
