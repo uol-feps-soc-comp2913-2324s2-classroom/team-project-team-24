cdd87011d38e058bca75e6f55b36f43b0113a7d4
Branson Tay <bransontay@gmail.com>
2024-03-20 23:21:50 +0000

    Began adding elements into containers
    Tried to change buttons to use new styleguide buttons but somehow they're not working

--- frontend/src/components/lists/TrailListItem.vue
+++ frontend/src/components/lists/TrailListItem.vue
@@ -1,45 +1,74 @@
 <template>
     <div class="outer" @click="viewTrail">
-        <div>
-            <h3>{{ trailName }}</h3>
-            <p>{{ trailDate }}</p>
-        </div>
-        <button @click.stop="downloadTrail">Download</button>
-        <button @click.stop="deleteTrail">Delete</button>
+      <!-- <div class="content"> -->
+        <h3>{{ trailName }}</h3>
+        <p>{{ trailDate }}</p>
+      <!-- </div> -->
+      <div class="buttons">
+        <primaryButton @click.stop="downloadTrail">Download</primaryButton>
+        <dangerButton @click.stop="deleteTrail">Delete</dangerButton>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import primaryButton from '../ui-components/primaryButton.vue';
+  import dangerButton from '../ui-components/dangerButton.vue';
 
-<script>
-export default {
+  export default {
     name: "TrailListItemComponent",
     props: {
-        trail: {
-            type: String
-        }
+      trail: {
+        type: String
+      }
     },
     data() {
-        return {
-            trailName: this.trail,
-            trailDate: "25/07/03",
-        };
+      return {
+        trailName: this.trail,
+        trailDate: "25/07/03",
+      };
     },
     methods: {
-        viewTrail() {
-            this.$router.push({path: "/mytrail", query: {trailName: this.trail}});
-        },
-        downloadTrail() {
-            console.log("downloadTrail");
-        },
-        deleteTrail() {
-            console.log("deleteTrail");
-        }
+      viewTrail() {
+        this.$router.push({ path: "/mytrail", query: { trailName: this.trail } });
+      },
+      downloadTrail() {
+        console.log("downloadTrail");
+      },
+      deleteTrail() {
+        console.log("deleteTrail");
+      }
     },
-};
-</script>
-
-<style>
-.outer {
+    components: {
+        primaryButton,
+        dangerButton
+    }
+  };
+  </script>
+  
+  <style scoped>
+  
+  .outer {
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #ddd; /* Add a border bottom for separation */
     cursor: pointer;
-}
-</style>
+  }
+  
+  .buttons button {
+    margin-left: 10px;
+    padding: 5px 10px;
+    border: none;
+    background-color: #007bff;
+    color: #fff;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  .buttons button:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
--- frontend/src/views/ActivityCenter.vue
+++ frontend/src/views/ActivityCenter.vue
@@ -24,14 +24,48 @@ export default {
 </script>
 
 <template>
-    <div class="activityCenterPageContainer">
-        <h1>Activity Center Page</h1>
+    <div class="activityCenterPageContainer p-5">
+        <div class="top-container">
         <GoalComponent />
-        <MapViewerComponent />
-        <OverallTrailStatsComponent />
-        <TrailListComponent v-bind:trails="trails"/>
+        </div>
+        <div class="main-container">
+            <div class="left-column">
+                <MapViewerComponent />
+            </div>
+            <div class="right-column">
+                <OverallTrailStatsComponent />
+            </div>
+        </div>
+        <div class="bottom-container">
+        <TrailListComponent :trails="trails" />
+        </div>
     </div>
 </template>
 
 <style scoped>
+.activityCenterPageContainer {
+  display: flex;
+  flex-direction: column;
+}
+
+.top-container {
+  margin-bottom: 20px;
+}
+
+.main-container {
+  display: flex;
+}
+
+.left-column {
+  flex: 2; /* Occupy 2/3 of the container */
+  margin-right: 10px; /* Add some space between columns */
+}
+
+.right-column {
+  flex: 1; /* Occupy 1/3 of the container */
+}
+
+.bottom-container {
+  margin-top: 20px;
+}
 </style>
