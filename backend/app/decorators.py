2eb16f952054c5a01a3b2a888f6247cfa230b0e7
Barracoda130 <lcompton-burnett@hotmail.com>
2024-03-27 18:00:21 +0100

    Fixed wrapper bug

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -2,11 +2,12 @@ from app.db_functions import get_user_membership_id
 from flask import jsonify
 from flask_jwt_extended import get_current_user
 
-def membership_required(fn):
+def membership_required(func):
     def wrapper():
         if get_user_membership_id(get_current_user().id) is None:
             return jsonify({"error": "User is not a member"}), 400
         else:
-            return fn()
-
+            return func()
+        
+    wrapper.__name__ = func.__name__
     return wrapper
