5a69c94a5c7225e3f25651b2baa8216d1c97499a
aes <edo.sunardi@gmail.com>
2024-04-27 16:12:00 +0100

    added nav to community page

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -2,7 +2,8 @@
 import UserListComponent from "@/components/lists/UserList.vue";
 import GroupListComponent from "@/components/lists/GroupList.vue";
 import NewFriendsComponent from "@/components/NewFriends.vue";
-import CreateGroupComponent from "@/components/forms/CreateGroup.vue";
+// import CreateGroupComponent from "@/components/forms/CreateGroup.vue";
+import topNavRailed from '@/components/ui-components/topNavRailedCommunity.vue';
 
 export default {
     name: "CommunityCenter",
@@ -10,21 +11,65 @@ export default {
         return {
             friendsIsShowing: true,
             groupsIsShowing: false,
-            createGroupIsShowing: false,
+            createGroupIsShowing: false, // This is now handled by the groupIsShowing
+            friendRequestsIsShowing: false,
+            addFriendsIsShowing: false,
             friends: ["friend1", "friend2", "friend3", "friend4", "friend5", "friend6", "friend7", "friend7"],
             groups: ["group1", "group2"]
         };
     },
     methods: {
+        navElementClicked(id) {
+            console.log(id);
+            id = parseInt(id);
+            switch (id) {
+                case 0:
+                    console.log("Friends");
+                    this.showFriends();
+                    break;
+                case 1:
+                    console.log("Groups");
+                    this.showGroups();
+                    break;
+                case 3:
+                    console.log("Friend Requests");
+                    this.showFriendRequests();
+                    break;
+                case 2:
+                    console.log("Add Friends");
+                    this.showAddFriends();
+                    break;
+                default:
+                    console.log("Unknown");
+                    break;
+            }
+            // console.log(event);
+        },
         showFriends() {
             this.friendsIsShowing = true;
             this.groupsIsShowing = false;
+            this.friendRequestsIsShowing = false;
+            this.addFriendsIsShowing = false;
         },
         showGroups() {
-            this.groupsIsShowing = true;
             this.friendsIsShowing = false;
+            this.groupsIsShowing = true;
+            this.friendRequestsIsShowing = false;
+            this.addFriendsIsShowing = false;
         },
-        createGroup() {
+        showFriendRequests() {
+            this.friendsIsShowing = false;
+            this.groupsIsShowing = false;
+            this.friendRequestsIsShowing = true;
+            this.addFriendsIsShowing = false;
+        },
+        showAddFriends() {
+            this.friendsIsShowing = false;
+            this.groupsIsShowing = false;
+            this.friendRequestsIsShowing = false;
+            this.addFriendsIsShowing = true;
+        },
+        createGroup() { // Deprecated
             /* For now, i hide groups and friends coz its easier,
              * but in future, we should have this overlay */
             this.groupsIsShowing = false;
@@ -41,26 +86,33 @@ export default {
         UserListComponent,
         GroupListComponent,
         NewFriendsComponent,
-        CreateGroupComponent,
+        // CreateGroupComponent,
+        topNavRailed,
     },
 };
 </script>
 
 <template>
-    <div class="communityPageContainer">
-        <h1>Community Page</h1>
+    <div class="communityPageContainer p-4">
+        <topNavRailed @NavElementClicked="navElementClicked"/>
         <div class="columns">
             <div>
-                <div style="display:flex;">
+                <!-- <div style="display:flex;">
                     <button @click="showFriends">Friends</button>
                     <button @click="showGroups">Groups</button>
-                </div>
+                </div> -->
                 <UserListComponent v-if="friendsIsShowing" v-bind:users="friends" />
-                <button @click="createGroup" v-if="groupsIsShowing" style="float:right; margin-right: 30px;">+</button>
+
+                <!-- <button @click="createGroup" v-if="groupsIsShowing" style="float:right; margin-right: 30px;">+</button> -->
                 <GroupListComponent v-if="groupsIsShowing" v-bind:groups="groups" />
-                <CreateGroupComponent v-if="createGroupIsShowing"/>
+                <!-- <CreateGroupComponent v-if="groupsIsShowing"/> -->
+
+                <NewFriendsComponent v-if="addFriendsIsShowing"/>
+                
+                <!-- friend requests compnent should go here-->
+
+
             </div>
-            <NewFriendsComponent />
         </div>
         
         
