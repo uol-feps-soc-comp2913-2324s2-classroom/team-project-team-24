9436d4c3add29b757b0efcb96f562e0e816ac7d2
samw-leeds <sc22sw@leeds.ac.uk>
2024-04-29 18:09:58 +0100

    added get owner membership data endpoint

--- backend/app/endpoints/owner.py
+++ backend/app/endpoints/owner.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import get_current_user, jwt_required
 from app import db, app
 from app.db_functions import *
 from app.models import User
+from app.decorators import *
+from datetime import date
 
 bp = Blueprint('owner', __name__, url_prefix='/owner')
 
@@ -18,5 +20,39 @@ bp = Blueprint('owner', __name__, url_prefix='/owner')
 #     }
 # }
 
+@bp.route('/get-owner-membership-data', methods=['GET'])
+@jwt_required()
+@membership_required
+def get_owner_membership_data():
+    response = {}
+    response["numUsers"] = len(User.query.all())
+
+    memberships = []
+    for membership in MembershipPlan.query.all():
+        memberships.append({
+            "id": membership.id,
+            "name": membership.name,
+            "payment_regularity": membership.payment_regularity,
+            "cost": membership.cost,
+            "numMembers": len(User.query.filter_by(membership_id=membership.id).all())
+        })
+
+    response["memberships"] = memberships
+    return jsonify(response)
+
 # what revenue levels they'll have weekly
 # expected annual revenue (calculated)
+
+@bp.route('/get-future-revenue', methods=['GET'])
+@jwt_required()
+@membership_required
+def get_future_revenue():
+    response = {}
+
+    today = date.today()
+    for i in range(52):
+        d = today + (i * datetime.timedelta(days=7))
+        response[str(d)] = 0
+
+    print(response)
+    return jsonify(response)
