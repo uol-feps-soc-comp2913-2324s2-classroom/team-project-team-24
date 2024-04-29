d733216cc088d006e62c4b38081c31aa7165e3c7
Shrey Bhatia <shrey.apply@gmail.com>
2024-04-30 02:29:26 +0530

    reformatted ActivityCenter.vue

--- frontend/src/views/ActivityCenter.vue
+++ frontend/src/views/ActivityCenter.vue
@@ -1,107 +1,113 @@
+<template>
+    <div class="activityCenterPageContainer p-2">
+        <div class="main-container">
+            <div class="map-view-column p-3">
+                <GoalComponent />
+                <MapViewerComponent
+                    v-bind:trailID="longestTrailID"
+                    :key="longestTrailID"
+                />
+            </div>
+            <div class="track-stats-column p-3">
+                <OverallTrailStatsComponent />
+            </div>
+        </div>
+        <div class="trails-container p-2">
+            <ListComponent v-bind:dataArray="trails" v-slot="slotProps">
+                <TrailListItemComponent
+                    :trail="slotProps.data"
+                    @trail-deleted="handleTrailDeleted"
+                />
+            </ListComponent>
+        </div>
+    </div>
+</template>
+
 <script>
-import MapViewerComponent from "@/components/MapViewer.vue";
-import GoalComponent from "@/components/Goal.vue";
-import OverallTrailStatsComponent from "@/components/OverallTrailStats.vue";
-import TrailListItemComponent from "@/components/lists/TrailListItem.vue";
-import ListComponent from "@/components/lists/List.vue";
-import axiosAuth from "@/api/axios-auth.js";
+import MapViewerComponent from '@/components/MapViewer.vue'
+import GoalComponent from '@/components/Goal.vue'
+import OverallTrailStatsComponent from '@/components/OverallTrailStats.vue'
+import TrailListItemComponent from '@/components/lists/TrailListItem.vue'
+import ListComponent from '@/components/lists/List.vue'
+import axiosAuth from '@/api/axios-auth.js'
 
 export default {
-  name: "ActivityCenter",
-  data() {
-    return {
-      trails: [],
-      longestTrailID: null,
-    };
-  },
-  methods: {
-    async getPageData() {
-      try {
-        const response1 = await axiosAuth.get('/trail/get-all');
-        this.trails = response1.data.trails;
+    name: 'ActivityCenter',
+    data() {
+        return {
+            trails: [],
+            longestTrailID: null,
+        }
+    },
+    methods: {
+        async getPageData() {
+            try {
+                const response1 = await axiosAuth.get('/trail/get-all')
+                this.trails = response1.data.trails
 
-        const response2 = await axiosAuth.get('/trail/get-longest');
-        this.longestTrailID = response2.data.trailID;
-      } catch (error) {
-        console.error('Error fetching page data:', error);
-      }
+                const response2 = await axiosAuth.get('/trail/get-longest')
+                this.longestTrailID = response2.data.trailID
+            } catch (error) {
+                console.error('Error fetching page data:', error)
+            }
+        },
+        handleTrailDeleted(trailId) {
+            // Remove the deleted trail from the trails array
+            this.trails = this.trails.filter((trail) => trail.id !== trailId)
+        },
     },
-    handleTrailDeleted(trailId) {
-      // Remove the deleted trail from the trails array
-      this.trails = this.trails.filter(trail => trail.id !== trailId);
+    components: {
+        MapViewerComponent,
+        GoalComponent,
+        OverallTrailStatsComponent,
+        TrailListItemComponent,
+        ListComponent,
     },
-  },
-  components: {
-    MapViewerComponent,
-    GoalComponent,
-    OverallTrailStatsComponent,
-    TrailListItemComponent,
-    ListComponent,
-  },
-  created() {
-    this.getPageData();
-  }
-};
+    created() {
+        this.getPageData()
+    },
+}
 </script>
-
-<template>
-  <div class="activityCenterPageContainer p-2">
-    <div class="main-container">
-      <div class="map-view-column p-3">
-        <GoalComponent />
-        <MapViewerComponent v-bind:trailID="longestTrailID" :key="longestTrailID" />
-      </div>
-      <div class="track-stats-column p-3">
-        <OverallTrailStatsComponent />
-      </div>
-    </div>
-    <div class="trails-container p-2">
-      <ListComponent v-bind:dataArray="trails" v-slot="slotProps">
-        <TrailListItemComponent :trail="slotProps.data" @trail-deleted="handleTrailDeleted" />
-      </ListComponent>
-    </div>
-  </div>
-</template>
 
 <style scoped>
 .activityCenterPageContainer {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 }
 
 .main-container {
-  display: flex;
-  height: 80vh;
+    display: flex;
+    height: 80vh;
 }
 
 .map-view-column {
-  flex: 2; /* Occupy 2/3 of the container */
-  margin-right: 10px; /* Add some space between columns */
-  background-color: var(--l1-color);
-  border-radius: var(--border-radius);
-  width: 100%;
-  height: 100%;
+    flex: 2; /* Occupy 2/3 of the container */
+    margin-right: 10px; /* Add some space between columns */
+    background-color: var(--l1-color);
+    border-radius: var(--border-radius);
+    width: 100%;
+    height: 100%;
 }
 
 .map-view-column iframe {
-  flex: 1; /* Allow the iframe to grow to fill available space */
-  width: 100%; /* Ensure the iframe takes full width of its container */
-  border: none; /* Remove iframe border */
+    flex: 1; /* Allow the iframe to grow to fill available space */
+    width: 100%; /* Ensure the iframe takes full width of its container */
+    border: none; /* Remove iframe border */
 }
 
 .track-stats-column {
-  flex: 1; /* Occupy 1/3 of the container */
-  margin-right: 10px; /* Add some space between columns */
-  background-color: var(--l1-color);
-  border-radius: var(--border-radius);
+    flex: 1; /* Occupy 1/3 of the container */
+    margin-right: 10px; /* Add some space between columns */
+    background-color: var(--l1-color);
+    border-radius: var(--border-radius);
 }
 
 .trails-container {
-  margin-top: 20px;
-  margin-right: 10px; /* Add some space between columns */
-  background-color: var(--l1-color);
-  border-radius: var(--border-radius);
-  width: 100%;
-  height: 100%;
+    margin-top: 20px;
+    margin-right: 10px; /* Add some space between columns */
+    background-color: var(--l1-color);
+    border-radius: var(--border-radius);
+    width: 100%;
+    height: 100%;
 }
 </style>
