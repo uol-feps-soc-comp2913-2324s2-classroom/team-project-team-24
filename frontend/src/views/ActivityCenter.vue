712a4ebdd92c82dc8c94148188e259dc34bee97c
Shrey Bhatia <shrey.apply@gmail.com>
2024-05-01 02:15:10 +0530

    updated handleTrailDeleted and reformatted file with Prettier

--- frontend/src/views/ActivityCenter.vue
+++ frontend/src/views/ActivityCenter.vue
@@ -3,8 +3,10 @@
         <div class="main-container">
             <div class="map-view-column p-3">
                 <GoalComponent />
-                <MapViewerComponent :selected-trails="selectedTrails"
-                ref="mapViewer" />
+                <MapViewerComponent
+                    :selected-trails="selectedTrails"
+                    ref="mapViewer"
+                />
             </div>
             <div class="track-stats-column p-3">
                 <OverallTrailStatsComponent />
@@ -24,76 +26,98 @@
 </template>
 
 <script>
-import MapViewerComponent from "@/components/MapViewer.vue";
-import GoalComponent from "@/components/Goal.vue";
-import OverallTrailStatsComponent from "@/components/OverallTrailStats.vue";
-import TrailListItemComponent from "@/components/lists/TrailListItem.vue";
-import ListComponent from "@/components/lists/List.vue";
-import axiosAuth from "@/api/axios-auth.js";
+import MapViewerComponent from '@/components/MapViewer.vue'
+import GoalComponent from '@/components/Goal.vue'
+import OverallTrailStatsComponent from '@/components/OverallTrailStats.vue'
+import TrailListItemComponent from '@/components/lists/TrailListItem.vue'
+import ListComponent from '@/components/lists/List.vue'
+import axiosAuth from '@/api/axios-auth.js'
 
 export default {
-  name: "ActivityCenter",
-  data() {
-    return {
-      trails: [],
-      longestTrailID: null,
-      selectedTrails: [],
-    };
-  },
-  methods: {
-    async getPageData() {
-      try {
-        const response1 = await axiosAuth.get("/trail/get-all");
-        this.trails = response1.data.trails;
+    name: 'ActivityCenter',
+    data() {
+        return {
+            trails: [],
+            longestTrailID: null,
+            selectedTrails: [],
+        }
+    },
+    methods: {
+        async getPageData() {
+            try {
+                const response1 = await axiosAuth.get('/trail/get-all')
+                this.trails = response1.data.trails
 
-        const response2 = await axiosAuth.get("/trail/get-longest");
-        this.longestTrailID = response2.data.trailID;
-      } catch (error) {
-        console.error("Error fetching page data:", error);
-      }
-    },
-    handleTrailDeleted(trailId) {
-      this.trails = this.trails.filter((trail) => trail.id !== trailId);
-    },
-    handleZoomToTrail(trailId) {
-      axiosAuth
-        .post("/trail/zoom-to-trail", {
-          trailID: trailId,
-          selectedTrailIDs: this.selectedTrails,
-        })
-        .then((response) => {
-          this.$refs.mapViewer.mapHtml = response.data.mapHtml;
-        })
-        .catch((error) => {
-          console.error("Error zooming to trail:", error);
-        });
-    },
-    handleTrailSelected({ trailId, checked }) {
-      console.log('ActivityCenter: handleTrailSelected called with trailId:', trailId, 'checked:', checked);
-      console.log('ActivityCenter: selectedTrails before update:', this.selectedTrails);
+                const response2 = await axiosAuth.get('/trail/get-longest')
+                this.longestTrailID = response2.data.trailID
+            } catch (error) {
+                console.error('Error fetching page data:', error)
+            }
+        },
+        handleTrailDeleted(trailId) {
+            // Remove the deleted trail from the trails array
+            this.trails = this.trails.filter((trail) => trail.id !== trailId)
 
-      if (checked) {
-        // Trail not already in the array, add it
-        this.selectedTrails = [...this.selectedTrails, trailId];
-      } else {
-        // Trail already in the array, remove it
-        this.selectedTrails = this.selectedTrails.filter(id => id !== trailId);
-      }
+            // Remove the deleted trail from the selectedTrails array
+            this.selectedTrails = this.selectedTrails.filter(
+                (id) => id !== trailId
+            )
 
-      console.log('ActivityCenter: selectedTrails after update:', this.selectedTrails);
+            // Trigger the fetchSelectedTrails method to update the map
+            this.$refs.mapViewer.fetchSelectedTrails()
+        },
+        handleZoomToTrail(trailId) {
+            axiosAuth
+                .post('/trail/zoom-to-trail', {
+                    trailID: trailId,
+                    selectedTrailIDs: this.selectedTrails,
+                })
+                .then((response) => {
+                    this.$refs.mapViewer.mapHtml = response.data.mapHtml
+                })
+                .catch((error) => {
+                    console.error('Error zooming to trail:', error)
+                })
+        },
+        handleTrailSelected({ trailId, checked }) {
+            console.log(
+                'ActivityCenter: handleTrailSelected called with trailId:',
+                trailId,
+                'checked:',
+                checked
+            )
+            console.log(
+                'ActivityCenter: selectedTrails before update:',
+                this.selectedTrails
+            )
+
+            if (checked) {
+                // Trail not already in the array, add it
+                this.selectedTrails = [...this.selectedTrails, trailId]
+            } else {
+                // Trail already in the array, remove it
+                this.selectedTrails = this.selectedTrails.filter(
+                    (id) => id !== trailId
+                )
+            }
+
+            console.log(
+                'ActivityCenter: selectedTrails after update:',
+                this.selectedTrails
+            )
+        },
     },
-  },
-  components: {
-    MapViewerComponent,
-    GoalComponent,
-    OverallTrailStatsComponent,
-    TrailListItemComponent,
-    ListComponent,
-  },
-  created() {
-    this.getPageData();
-  },
-};
+    components: {
+        MapViewerComponent,
+        GoalComponent,
+        OverallTrailStatsComponent,
+        TrailListItemComponent,
+        ListComponent,
+    },
+    created() {
+        this.getPageData()
+    },
+}
 </script>
 
 <style scoped>
