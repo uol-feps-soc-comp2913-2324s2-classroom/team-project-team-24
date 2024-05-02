a6846b2e51fcf3d1593718acb4c5da231c688323
Branson Tay <bransontay@gmail.com>
2024-05-02 17:02:19 +0100

    Added register command to cypress

--- frontend/cypress/e2e/registerForm.cy.js
+++ frontend/cypress/e2e/registerForm.cy.js
@@ -1,20 +1,21 @@
 describe('RegisterFormComponent', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/register')
+        cy.register(); // Log in before each test  
     })
 
-    it('shoule register a new user', () => {
-        cy.get('#username').type('newuser')
-        cy.get('#email').type('newuser@example.com')
-        cy.get('#password').type('password123')
-        cy.get('#confirmPassword').type('password123')
+    it('should register a new user', () => {
+        cy.contains('label', 'Username').siblings('input').type('chick');
+        cy.contains('label', 'Email').siblings('input').type('chick@example.com');
+        cy.contains('label', 'Password').siblings('input').type('Password123');
+        cy.contains('label', 'Confirm Password').siblings('input').type('Password123');
 
         // continue button
         cy.contains('button', 'Continue').click()
 
-        //step 2
-        cy.get('#gender').type('male')
-        cy.get('#age').type('18')
+        // step 2 of register
+        cy.contains('label', 'Gender').siblings('input').type('male');
+        cy.contains('label', 'Age').siblings('input').type('18');
         cy.contains('button', 'Register').click()
     })
 
